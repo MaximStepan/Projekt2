0d3e561ee5164fd8f5d4a629bf5f29ebeb236670
Maxim Stepan <D21133@oaopava.cz>
2023-11-16 09:38:01 +0100

    changes

---
+++ test.py
@@ -0,0 +1,10 @@
+a=float(input("Zadej stranu a"))
+b=float(input("Zadej stranu b"))
+c=float(input("Zadej stranu c"))
+
+p=2*(a*b+b*c+a*c)
+V=a*b*c
+
+print(f"Zadaný kvádr má povrch {p} a obsah {V}")
+
+
